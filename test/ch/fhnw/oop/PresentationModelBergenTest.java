4bf815a1a156ca5db11b69ad8f72ff93dadca6c6
anhichang <anhi.chang@students.fhnw.ch>
2015-11-27 16:37:13 +0100

    version:27.

---
+++ test/ch/fhnw/oop/PresentationModelBergenTest.java
@@ -0,0 +1,21 @@
+package ch.fhnw.oop;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertSame;
+import static junit.framework.Assert.assertTrue;
+/**
+ * Created by ANhi on 11/22/2015.
+ */
+public class PresentationModelBergenTest {
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
